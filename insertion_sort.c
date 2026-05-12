288ed866ecf726e2353c24d3b579eb2973819292
bishalsarker4997-boop <bishalsarker4997@gmail.com>
2026-05-12 11:42:39 +0600

    Add files via upload

---
+++ insertion_sort.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = 5;
+
+    int i, j, key;
+
+    printf("Original Array:\n");
+
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    // Insertion Sort
+    for(i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+
+        // বড় element গুলো এক ঘর পিছাবে
+        while(j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+
+    printf("\nSorted Array:\n");
+
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
